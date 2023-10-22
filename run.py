28961d1a08ac7e2397dd0b57955d9c38624d20d3
iiJozza <>
2023-10-22 07:33:31 +0000

    Added grid. Size of grid is dependent on user input

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def create_grid(size):
+    if size < 1 or size > 9:
+        raise ValueError("Grid size must be between 1 and 9")
+
+    grid = [["·"] * size for _ in range(size)]
+
+    return grid
+
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(row))
+
+if __name__ == "__main__":
+    while True:
+        try:
+            size = int(input("Enter the grid size (1-9): "))
+            grid = create_grid(size)
+            print_grid(grid)
+            break
+        except ValueError:
+            print("Please enter a valid number for the grid size.")
