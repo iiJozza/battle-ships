daa504d0bd1558cfe1e32bd5baf7cc1af00bcb8c
iiJozza <>
2023-10-25 07:56:37 +0000

    indented text 8 spaces

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import time
 import pyfiglet
 from colorama import Fore, Back, Style
 
+extra_spaces = " " * 8
+
 
 class BattleshipGame:
     def __init__(self, grid_size, num_of_ships):
@@ -45,18 +47,20 @@ class BattleshipGame:
         """
         Prints the game boards, while hiding the computer's ships.
         """
-        print("The Player's Board:")
+
+        print(f"{extra_spaces}The Player's Board:")
 
         # Prints the player's game board with row and column labels
-        print("    " + " ".join(chr(65 + i) for i in range(self.grid_size)))
-        print("  +" + " -" * self.grid_size + " +")
+        print(f"{extra_spaces}    " + " \
+".join(chr(65 + i) for i in range(self.grid_size)))
+        print(f"{extra_spaces}  +" + " -" * self.grid_size + " +")
 
         for i, row in enumerate(self.player_board):
-            print(f"{i + 1} | " + " ".join(row) + " |")
+            print(f"{extra_spaces}{i + 1} | " + " ".join(row) + " |")
 
-        print("  +" + " -" * self.grid_size + " +")
+        print(f"{extra_spaces}  +" + " -" * self.grid_size + " +")
 
-        print("The Computer's Board:")
+        print(f"{extra_spaces}The Computer's Board:")
 
         # Hides the computers ships
         if hide_computer_ships:
@@ -68,13 +72,14 @@ class BattleshipGame:
             computer_board_to_display = self.computer_board
 
         # Prints the computer's game board with row and column labels
-        print("    " + " ".join(chr(65 + i) for i in range(self.grid_size)))
-        print("  +" + " -" * self.grid_size + " +")
+        print(f"{extra_spaces}    " + " \
+".join(chr(65 + i) for i in range(self.grid_size)))
+        print(f"{extra_spaces}  +" + " -" * self.grid_size + " +")
 
         for i, row in enumerate(computer_board_to_display):
-            print(f"{i + 1} | " + " ".join(row) + " |")
+            print(f"{extra_spaces}{i + 1} | " + " ".join(row) + " |")
 
-        print("  +" + " -" * self.grid_size + " +")
+        print(f"{extra_spaces}  +" + " -" * self.grid_size + " +")
 
     def place_ships(self, board):
         """
@@ -97,24 +102,27 @@ class BattleshipGame:
         """
         # Failsafe if the user picked the same space
         if self.computer_board[row][col] == "M":
-            print("You already guessed that spot, try again")
+            print(f"{extra_spaces}You already guessed that spot, try again")
             return False
 
         # Checks if the player hits a ship, and if the player won
         if self.computer_board[row][col] == "0":
-            print(Fore.RED + f"\nThat is a " "hit!" + Style.RESET_ALL)
+            print(Fore.RED + f"{extra_spaces}\nThat is a " "hit!\
+" + Style.RESET_ALL)
             self.computer_ships_remaining -= 1
 
             if self.computer_ships_remaining == 0:
-                print("Congratulations!!!")
+                print(f"{extra_spaces}Congratulations!!!")
             else:
-                print(f"There're {self.computer_ships_remaining} ships left")
+                print(f"{extra_spaces}There're \
+{self.computer_ships_remaining} \
+ships left")
             self.computer_board[row][col] = "X"
             time.sleep(2)
 
         # Checks if the player missed
         else:
-            print(Fore.BLUE + "\nMissed! Better luck next time.\
+            print(Fore.BLUE + f"{extra_spaces}\nMissed! Better luck next time.\
 " + Style.RESET_ALL)
             self.computer_board[row][col] = "M"
             time.sleep(2)
@@ -134,17 +142,21 @@ class BattleshipGame:
 
         # Checks if computer won/hit/miss a ship. Updates grid afterwards
         if self.player_board[row][col] == "0":
-            print(Fore.RED + f"Computer hit your ship at\
+            print(Fore.RED + f"{extra_spaces}Computer hit your ship at\
  ({chr(65 + col)}, {row + 1})" + Style.RESET_ALL)
             self.player_ships_remaining -= 1
             if self.player_ships_remaining == 0:
-                print("Oh no! The computer sank all your ships!")
+                print(f"{extra_spaces}Oh no! The computer sank \
+all your ships!")
             else:
-                print(f"You've got {self.player_ships_remaining} ships left\n")
+                print(f"{extra_spaces}You've got \
+{self.player_ships_remaining} \
+ships left\n")
             self.player_board[row][col] = "X"
             time.sleep(2)
         else:
-            print(Fore.BLUE + f"\nComputer has missed! They shot at\
+            print(Fore.BLUE + f"{extra_spaces}\nComputer has missed! \
+They shot at\
 ({chr(65 + col)}, {row + 1})" + Style.RESET_ALL)
             self.player_board[row][col] = "M"
             time.sleep(2)
@@ -160,25 +172,27 @@ class BattleshipGame:
                 try:
                     # Row
                     max_char = chr(64 + self.grid_size)
-                    guess_row = input(f"Guess a row (A-{max_char}): ").upper()
+                    guess_row = input(f"{extra_spaces}\
+Guess a row between (A-{max_char}): ").upper()
 
                     # Checks if the player wants to exit the game
                     if guess_row.lower() == "exit":
-                        print("Exiting the game. Goodbye!")
+                        print(f"{extra_spaces}Exiting the game. Goodbye!")
                         return
 
                     # Checks if it's a letter
                     if not guess_row.isalpha():
-                        print(f"Invalid input. Please enter a letter between \
-(A-{max_char}).")
+                        print(f"{extra_spaces}Invalid input. \
+Please enter a letter between (A-{max_char}).")
                         continue
 
                     # Column
-                    guess_col = input(f"Column (1-{self.grid_size}): ")
+                    guess_col = input(f"{extra_spaces}\
+Guess a column between (1-{self.grid_size}): ")
 
                     # Checks if the player wants to exit the game
                     if guess_col.lower() == "exit":
-                        print("Exiting the game. Goodbye!")
+                        print(f"{extra_spaces}Exiting the game. Goodbye!")
                         return
 
                     # Converts from str -> int
@@ -192,7 +206,7 @@ class BattleshipGame:
                         or int(guess_col) < 1
                         or int(guess_col) > self.grid_size
                     ):
-                        print("That's outside the grid")
+                        print(f"{extra_spaces}That's outside the grid")
                         continue
 
                     if self.player_guess(ord(guess_row) - 65, guess_col - 1):
@@ -200,13 +214,14 @@ class BattleshipGame:
 
                 # Error message for invalid column input
                 except ValueError:
-                    print(f"Enter a valid number between 1 -{self.grid_size}!")
+                    print(f"{extra_spaces}Enter a valid number between 1 -\
+{self.grid_size}!")
 
             #  Checks if player won and ends the game if true.
             if not any("0" in row for row in self.computer_board):
                 break
 
-            print("\nThe Computer's Turn")
+            print(f"{extra_spaces}\nThe Computer's Turn")
             self.computer_guess()
 
             # Checks if computer won and ends the game if true
@@ -219,11 +234,12 @@ class BattleshipGame:
         Small loading animation with dots
         """
         for _ in range(seconds):
-            print(". ", end="", flush=True)
+            print(f"{extra_spaces}. ", end="", flush=True)
             time.sleep(0.7)
         print()
 
 # Starting page before game start with introduction and rules
+
 
 if __name__ == "__main__":
     from colorama import Fore, Back, Style, init
@@ -272,20 +288,22 @@ Symbolizes that you missed and hit nothing
         while True:
             try:
                 # Obtains and validates the grid size input from the user
-                size = int(input("Enter the grid size (1-9): "))
+                size = int(input(f"{extra_spaces}Enter the grid size (1-9): "))
                 if size < 1 or size > 9:
-                    print("Grid size should be between 1 and 9.")
+                    print(f"{extra_spaces}Grid size should be \
+between 1 and 9.")
                     continue
                 break
             except ValueError:
-                print("Please enter a valid number")
+                print(f"{extra_spaces}Please enter a valid number")
 
         while True:
             try:
                 # Validates ship count input
-                num_of_ships = int(input("Enter the number of ships: "))
+                num_of_ships = int(input(f"{extra_spaces}\
+Enter the number of ships: "))
                 if num_of_ships <= 0 or num_of_ships > size * size:
-                    print("Ship amount entered is invalid")
+                    print(f"{extra_spaces}Ship amount entered is invalid")
                     continue
                 game = BattleshipGame(size, num_of_ships)
                 break
@@ -293,20 +311,23 @@ Symbolizes that you missed and hit nothing
             except ValueError as e:
                 # Error message for unvalid input
                 if "Number of ships can't exceed the grid size" in str(e):
-                    print("You've entered more ships than the grid can handle")
+                    print(f"{extra_spaces}You've entered more ships \
+than the grid can handle")
                 else:
-                    print("Please enter a valid number")
+                    print(f"{extra_spaces}Please enter a valid number")
 
         # Continues or ends the game based on the player's input.
         game.play()
         while True:
-            play_again = input("Do you want to play again? (yes/no): ")
+            play_again = input(f"{extra_spaces}Do you want to play again? \
+(yes/no): ")
             if play_again.lower() == "yes":
-                print("Perfect! Let me start a new game for you.")
+                print(f"{extra_spaces}Perfect! \
+Let me start a new game for you.")
                 game.loading_animation(3)
                 break
             elif play_again.lower() == "no":
-                print("Thank you for playing.")
+                print(f"{extra_spaces}Thank you for playing.")
                 exit()
             else:
-                print("Please choose 'yes' or 'no'")
+                print(f"{extra_spaces}Please choose 'yes' or 'no'")
